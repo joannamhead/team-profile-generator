3c743a10ac910be38275aaab25d059c8503255a0
joannamhead <joannamhead@hotmail.co.uk>
2024-02-13 15:04:36 +0000

    defined manager class

--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(name, id, email) {
+    this.name = "Jo"
+    this.id = 3;
+    this.email = "jo@fakemail.com";
+    }
+    getName() {
+        console.log(`Name: ${this.name}`);
+    }
+
+    getId() {
+        console.log(`ID: ${this.id}`);
+    }
+
+    getEmail() {
+        console.log(`Email: ${this.email}`);
+    }
+
+    getRole() {
+        console.log(`Role: Manager`);
+    }
+}
